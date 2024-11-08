e9cbae8e69e80613658025da1aff75dc5ebb6470
svirekat <eketerinasv@gmail.com>
2024-11-08 23:02:26 +0300

    Update calcscript.js
    Signed-off-by: svirekat <eketerinasv@gmail.com>

--- calculator/calcscript.js
+++ calculator/calcscript.js
@@ -1,4 +1,4 @@
-function calculateCost(event) {
+function calculateCost() {
       const quantityInput = document.getElementById("quantity");
       const quantity = document.getElementById("quantity").value;
       const productPrice = document.getElementById("product").value;
@@ -13,14 +13,13 @@ function calculateCost(event) {
       else {
         quantityInput.classList.remove("error");
       }
+
       const cost = quantity * productPrice;
       document.getElementById("cost").innerHTML = "Стоимость заказа: " + cost + " руб.";
       document.getElementById("error").innerHTML = ""; // Очищаем сообщение об ошибке
-      event.preventDefault();
-      alert("click");
     }
-window.addEventListener('DOMContentLoaded', function (event) {
+    window.addEventListener('DOMContentLoaded', function (event) {
       console.log("DOM fully loaded and parsed");
       let b = document.getElementById("button");
       b.addEventListener("click", calculateCost);
-};
+    });
